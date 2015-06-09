597b67e641a12c01dda723991cb85de5d57a2cdf
MathiasB <git@denbeke.be>
2015-06-09 18:50:15 +0200

    Initial Commit

---
+++ Liveblog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Liveblog
+//
+//  Created by Mathias Beke on 9/06/15.
+//  Copyright © 2015 DenBeke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
